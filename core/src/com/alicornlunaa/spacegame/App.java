7188a274ed7fb67e32d72fdd457c07b1f8729b5f
Luna <alicornlunaa@gmail.com>
2022-09-03 17:10:06 -0500

    UI work

--- core/src/com/alicornlunaa/spacegame/App.java
+++ core/src/com/alicornlunaa/spacegame/App.java
@@ -4,36 +4,87 @@ import com.alicornlunaa.spacegame.objects.Ground;
 import com.alicornlunaa.spacegame.objects.Ship;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class App extends ApplicationAdapter {
-	World world;
-	Stage stage;
-	Body body;
+	private World world;
+	private Stage gameStage;
+	private Box2DDebugRenderer debugRenderer;
+	private InputMultiplexer inputs;
 
-	Box2DDebugRenderer debug;
+	// https://libgdx.com/wiki/graphics/2d/scene2d/table
+	private Skin skin;
+	private Table ui;
+	private Table pauseMenu;
+
+	private boolean paused;
+	private boolean debug;
 	
 	@Override
 	public void create(){
 		world = new World(new Vector2(0, -600.f), true);
-		stage = new Stage(new ScreenViewport());
-		Gdx.input.setInputProcessor(stage);
+		gameStage = new Stage(new ScreenViewport());
+		debugRenderer = new Box2DDebugRenderer();
 
-		debug = new Box2DDebugRenderer();
+		skin = new Skin(Gdx.files.internal("skins/default/skin/uiskin.json"));
+		ui = new Table();
+		pauseMenu = new Table();
 
-		stage.addActor(new Ship(world, 640/2, 250, 15));
-		stage.addActor(new Ground(world, 640/2, 20, 500, 15));
+		paused = false;
+		debug = true;
+
+		inputs = new InputMultiplexer();
+		inputs.addProcessor(new InputAdapter(){
+			@Override
+			public boolean keyDown(int keyCode){
+				switch(keyCode){
+				case Keys.ESCAPE:
+					paused = !paused;
+					return true;
+
+				case Keys.F3:
+					debug = !debug;
+					ui.setDebug(debug);
+					pauseMenu.setDebug(debug);
+					return true;
+				}
+
+				return false;
+			}
+		});
+		inputs.addProcessor(gameStage);
+		Gdx.input.setInputProcessor(inputs);
+
+		ui.setFillParent(true);
+		ui.setDebug(true);
+		gameStage.addActor(ui);
+
+		pauseMenu.setFillParent(true);
+		pauseMenu.setDebug(true);
+		pauseMenu.add(new Label("PAUSED", skin));
+		pauseMenu.row();
+		pauseMenu.add(new TextButton("QUIT", skin)).width(100).height(40);
+		gameStage.addActor(pauseMenu);
+
+		gameStage.addActor(new Ship(world, 640/2, 250, 15));
+		gameStage.addActor(new Ground(world, 640/2, 20, 500, 15));
 	}
 
 	@Override
 	public void resize(int width, int height){
-		stage.getViewport().update(width, height, true);
+		gameStage.getViewport().update(width, height, true);
 	}
 
 	@Override
@@ -41,16 +92,23 @@ public class App extends ApplicationAdapter {
 		float delta = Gdx.graphics.getDeltaTime();
 
 		ScreenUtils.clear(0.1f, 0.1f, 0.1f, 1.f);
-		stage.act(delta);
-		stage.draw();
 
-		debug.render(world, stage.getCamera().combined);
+		pauseMenu.setVisible(paused);
 
-		world.step(1/60.f, 6, 2);
+		gameStage.act(delta);
+		gameStage.draw();
+
+		if(debug){
+			debugRenderer.render(world, gameStage.getCamera().combined);
+		}
+
+		if(!paused){
+			world.step(1/60.f, 6, 2);
+		}
 	}
 	
 	@Override
 	public void dispose(){
-		stage.dispose();
+		gameStage.dispose();
 	}
 }
