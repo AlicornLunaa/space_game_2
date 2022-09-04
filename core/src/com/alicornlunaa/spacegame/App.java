0152d852a75b384eec038ec8d434b7af65682f07
Luna <alicornlunaa@gmail.com>
2022-09-04 01:23:32 -0500

    Updated App

--- core/src/com/alicornlunaa/spacegame/App.java
+++ core/src/com/alicornlunaa/spacegame/App.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
@@ -32,8 +33,13 @@ public class App extends ApplicationAdapter {
 	private Table ui;
 	private Table pauseMenu;
 
+	private TextButton rcsButton;
+	private TextButton sasButton;
+
 	private boolean paused;
 	private boolean debug;
+	private boolean rcs;
+	private boolean sas;
 	
 	@Override
 	public void create(){
@@ -47,6 +53,8 @@ public class App extends ApplicationAdapter {
 
 		paused = false;
 		debug = true;
+		rcs = false;
+		sas = false;
 
 		inputs = new InputMultiplexer();
 		inputs.addProcessor(new InputAdapter(){
@@ -61,6 +69,16 @@ public class App extends ApplicationAdapter {
 					debug = !debug;
 					ui.setDebug(debug);
 					pauseMenu.setDebug(debug);
+					return true;
+
+				case Keys.R:
+					rcs = !rcs;
+					rcsButton.setColor(rcs ? Color.GREEN : Color.RED);
+					return true;
+
+				case Keys.T:
+					sas = !sas;
+					sasButton.setColor(sas ? Color.GREEN : Color.RED);
 					return true;
 				}
 
@@ -79,6 +97,16 @@ public class App extends ApplicationAdapter {
 
 		ui.setFillParent(true);
 		ui.setDebug(true);
+		rcsButton = new TextButton("RCS", skin);
+		sasButton = new TextButton("SAS", skin);
+		rcsButton.setPosition(640 - 128, 480 - 32);
+		rcsButton.setSize(64, 32);
+		rcsButton.setColor(Color.RED);
+		gameStage.addActor(rcsButton);
+		sasButton.setPosition(640 - 64, 480 - 32);
+		sasButton.setSize(64, 32);
+		sasButton.setColor(Color.RED);
+		gameStage.addActor(sasButton);
 		gameStage.addActor(ui);
 
 		pauseMenu.setFillParent(true);
@@ -114,6 +142,14 @@ public class App extends ApplicationAdapter {
 
 		gameStage.act(delta);
 		gameStage.draw();
+
+		if(rcs){
+
+		}
+		
+		if(sas){
+
+		}
 
 		if(debug){
 			debugRenderer.render(world, gameStage.getCamera().combined);
