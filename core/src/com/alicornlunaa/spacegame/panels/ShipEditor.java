d1d35391fb71f8cb485720b332bcb7e0a4f17538
Luna <alicornlunaa@gmail.com>
2022-10-28 20:32:56 -0500

    Fixed ship editor cursor of selected item not showing up

--- core/src/com/alicornlunaa/spacegame/panels/ShipEditor.java
+++ core/src/com/alicornlunaa/spacegame/panels/ShipEditor.java
@@ -13,6 +13,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -26,6 +27,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
@@ -44,6 +46,9 @@ public class ShipEditor extends Stage {
     private Stack partsStack;
     private EditorPane editor;
 
+    private SpriteDrawable ghostedPart;
+    private String selectedPart;
+
     // Constructor
     public ShipEditor(final Assets manager, final ArrayList<Stage> stages, final Skin skin, final PartManager partManager){
         super(new ScreenViewport());
@@ -54,6 +59,9 @@ public class ShipEditor extends Stage {
 
         selectedCategory = "AERO";
         cateogryIndices = new HashMap<String, Integer>();
+
+        ghostedPart = new SpriteDrawable(new Sprite());
+        selectedPart = "";
 
         ui = new Table(skin);
         ui.setFillParent(true);
@@ -116,19 +124,23 @@ public class ShipEditor extends Stage {
 
                 // Each entry is a category
                 JSONObject btnData = partManager.get(entry, objKey);
-                TextureRegionDrawable texture = new TextureRegionDrawable(new TextureRegion(
+                final TextureRegion region = new TextureRegion(
                     manager.get(partManager.get(entry, objKey).getString("texture"), Texture.class),
                     btnData.getJSONObject("uv").getInt("x"),
                     btnData.getJSONObject("uv").getInt("y"),
                     btnData.getJSONObject("uv").getInt("width"),
                     btnData.getJSONObject("uv").getInt("height")
-                ));
+                );
+                TextureRegionDrawable texture = new TextureRegionDrawable(region);
                 ImageButton btn = new ImageButton(texture);
 
                 btn.addListener(new ChangeListener(){
                     @Override
                     public void changed(ChangeEvent event, Actor actor){
-                        //! SPAWN PART HERE
+                        // TODO: SPAWN PART HERE
+                        ((ShipEditor)event.getStage()).selectedPart = objKey;
+                        ((ShipEditor)event.getStage()).ghostedPart.getSprite().setRegion(region);
+                        ((ShipEditor)event.getStage()).ghostedPart.getSprite().setSize(region.getRegionWidth(), region.getRegionHeight());
                     }
                 });
 
@@ -166,6 +178,31 @@ public class ShipEditor extends Stage {
     }
 
     // Functions
+    @Override
+    public void act(float delta){
+        super.act(delta);
+
+        // Set the cursor object to the mouse if an object was selected
+        if(!selectedPart.equals("")){
+            ghostedPart.getSprite().setPosition(Gdx.input.getX(), Gdx.input.getY());
+        }
+    }
+
+    @Override
+    public void draw(){
+        super.draw();
+
+        // Set the cursor object to the mouse if an object was selected
+        if(!selectedPart.equals("")){
+            Sprite s = ghostedPart.getSprite();
+
+            this.getBatch().setProjectionMatrix(this.getCamera().combined);
+            this.getBatch().begin();
+            ghostedPart.draw(this.getBatch(), s.getX(), this.getHeight() - s.getY(), s.getWidth(), s.getHeight());
+            this.getBatch().end();
+        }
+    }
+
     @Override
     public void dispose(){
         super.dispose();
