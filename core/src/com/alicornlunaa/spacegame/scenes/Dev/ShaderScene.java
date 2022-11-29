b6e99b3c12af36e07e10db04ba13eafa51f47f15
Luna <alicornlunaa@gmail.com>
2022-11-29 14:24:52 -0600

    Organized lighting code into own class

--- core/src/com/alicornlunaa/spacegame/scenes/Dev/ShaderScene.java
+++ core/src/com/alicornlunaa/spacegame/scenes/Dev/ShaderScene.java
@@ -1,18 +1,13 @@
 package com.alicornlunaa.spacegame.scenes.Dev;
 
 import com.alicornlunaa.spacegame.App;
-import com.badlogic.gdx.Input.Keys;
+import com.alicornlunaa.spacegame.objects.Lights.PointLight;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Pixmap.Format;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.graphics.Texture.TextureWrap;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.graphics.glutils.FrameBuffer;
-import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
@@ -24,7 +19,6 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 public class ShaderScene implements Screen {
 
     // private final App game;
-    private final App game;
     private OrthographicCamera cam;
     private Stage stage;
     private Label fpsCounter;
@@ -35,16 +29,9 @@ public class ShaderScene implements Screen {
     private float lastUpdate = 0.0f;
 
     private Texture casterTexture;
-
-    private Vector2 lightPos = new Vector2(0, 0);
-    private int lightResolution = 512;
-    private FrameBuffer occlusionFBO;
-    private TextureRegion occlusionMap;
-    private FrameBuffer shadowMapFBO;
-    private TextureRegion shadowMap;
+    private PointLight light;
 
     public ShaderScene(final App game){
-        this.game = game;
         stage = new Stage(new ScreenViewport());
 
         fpsCounter = new Label("FPS: N/A", game.skin);
@@ -60,17 +47,7 @@ public class ShaderScene implements Screen {
         cam.update();
 
         casterTexture = new Texture(Gdx.files.internal("textures/ui/caster.png"));
-
-        occlusionFBO = new FrameBuffer(Format.RGBA8888, lightResolution, lightResolution, false);
-        occlusionMap = new TextureRegion(occlusionFBO.getColorBufferTexture());
-        occlusionMap.flip(false, true);
-        
-        shadowMapFBO = new FrameBuffer(Format.RGBA8888, lightResolution, 1, false);
-        Texture shadowTexture = shadowMapFBO.getColorBufferTexture();
-        shadowTexture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-        shadowTexture.setWrap(TextureWrap.Repeat, TextureWrap.Repeat);
-        shadowMap = new TextureRegion(shadowTexture);
-        shadowMap.flip(false, true);
+        light = new PointLight(game, new Vector2());
 
         // Controls
         stage.addListener(new InputListener(){
@@ -104,51 +81,24 @@ public class ShaderScene implements Screen {
         }
 
         Batch batch = stage.getBatch();
-        ShaderProgram shadowShader = game.manager.get("shaders/shadow_map", ShaderProgram.class);
-        ShaderProgram lightShader = game.manager.get("shaders/light", ShaderProgram.class);
 
-        lightPos.set(Gdx.input.getX(), Gdx.input.getY());
-        lightPos = stage.screenToStageCoordinates(lightPos);
+        light.getPosition().set(Gdx.input.getX(), Gdx.input.getY());
+        light.getPosition().set(stage.screenToStageCoordinates(light.getPosition()));
 
-        occlusionFBO.begin();
-        ScreenUtils.clear(0, 0, 0, 1);
-        cam.setToOrtho(false, occlusionFBO.getWidth(), occlusionFBO.getHeight());
-        cam.translate(lightPos.x - lightResolution / 2.f, lightPos.y - lightResolution / 2.f);
-        cam.update();
-        batch.setProjectionMatrix(cam.combined);
-        batch.setShader(null);
-        batch.begin();
+        light.beginOcclusion(batch);
         batch.draw(casterTexture, 0, 0);
-        batch.end();
-        occlusionFBO.end();
+        light.endOcclusion(batch);
 
-        shadowMapFBO.begin();
-        ScreenUtils.clear(0, 0, 0, 1);
-        batch.setShader(shadowShader);
-        batch.begin();
-        shadowShader.setUniformf("u_resolution", lightResolution, lightResolution);
-        cam.setToOrtho(false, shadowMapFBO.getWidth(), shadowMapFBO.getHeight());
-        batch.setProjectionMatrix(cam.combined);
-        batch.draw(occlusionFBO.getColorBufferTexture(), 0, 0, lightResolution, shadowMapFBO.getHeight());
-        batch.end();
-        shadowMapFBO.end();
-
-        batch.setShader(lightShader);
         batch.begin();
         cam.setToOrtho(false);
         cam.update();
         batch.setProjectionMatrix(cam.combined);
-        lightShader.setUniformf("u_resolution", lightResolution, lightResolution);
-        batch.draw(shadowMap.getTexture(), lightPos.x - lightResolution / 2.f, lightPos.y - lightResolution / 2.f, lightResolution, lightResolution);
+        light.drawLight(batch);
         batch.end();
 
         batch.setShader(null);
         batch.begin();
         batch.draw(casterTexture, 0, 0);
-		// batch.setColor(Color.BLACK);
-		// batch.draw(occlusionMap, stage.getWidth() - lightResolution, 0);
-		// batch.setColor(Color.WHITE);
-		// batch.draw(shadowMap, stage.getWidth() - lightResolution, lightResolution + 5, lightResolution, 15);
         batch.end();
         
         fpsCounter.setText(String.valueOf(avgFps));
--- core/src/com/alicornlunaa/spacegame/scenes/SpaceScene/SpacePanel.java
+++ core/src/com/alicornlunaa/spacegame/scenes/SpaceScene/SpacePanel.java
@@ -58,10 +58,10 @@ public class SpacePanel extends Stage {
         game.player.drive(ship);
 
         // ship.getBody().applyForceToCenter(10, 6, true);
-        // universe.getCelestial(2).getBody().applyLinearImpulse(0, 350, universe.getCelestial(2).getBody().getWorldCenter().x, universe.getCelestial(2).getBody().getWorldCenter().y, true);
+        universe.getCelestial(2).getBody().applyLinearImpulse(0, 250, universe.getCelestial(2).getBody().getWorldCenter().x, universe.getCelestial(2).getBody().getWorldCenter().y, true);
 
         //! TESTING
-        ship.setPosition(1000, 0);
+        // ship.setPosition(1000, 0);
 
         // Controls
         this.addListener(new InputListener(){
