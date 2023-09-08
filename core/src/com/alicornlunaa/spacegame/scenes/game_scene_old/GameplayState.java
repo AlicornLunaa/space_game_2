0c01cd220296cd3626c426ec3e1c1e55d0c57863
AlicornLunaa <alicornlunaa@gmail.com>
2023-09-08 14:11:17 -0500

    fuck

--- core/src/com/alicornlunaa/spacegame/scenes/game_scene_old/GameplayState.java
+++
@@ -1,8 +0,0 @@
-package com.alicornlunaa.spacegame.scenes.game_scene_old;
-
-public enum GameplayState {
-    SPACE, // The view in space
-    MAP, // The view for the map
-    SHIP, // The view inside a ship
-    PLANET // The view on a planet
-}
